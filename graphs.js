c7161f79b281684a53d3d28d9afd80469b55fbad
sofiene sp <sofienesp260@gmail.com>
2018-10-09 12:14:24 +0200

    second commit

--- graphs.js
+++ graphs.js
@@ -1,6 +1,4 @@
 
-import Chart from 'Chart.js';
-
 function csvJSON(csv){
 
     var lines=csv.split("\n");
@@ -27,44 +25,33 @@ function csvJSON(csv){
 }
 
 function bar_chart(){
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
+    new Chart(document.getElementById("myChart"), {
         type: 'bar',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+          labels: ["1900", "1950", "1999", "2050"],
+          datasets: [
+            {
+              label: "Africa",
+              backgroundColor: "#3e95cd",
+              data: [133,221,783,2478]
+            }, {
+              label: "Europe",
+              backgroundColor: "#8e5ea2",
+              data: [408,547,675,734]
+            }
+          ]
         },
         options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
+          title: {
+            display: true,
+            text: 'Population growth (millions)'
+          }
         }
     });
+    
 }
+
+bar_chart()
 
 json_file = csvJSON("juin_processed.csv");
 var x = 260
