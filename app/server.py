a5b7bdc1b0a01b2606d5445ec7ff088b27f6c140
Cavallones <q.sofian@gmail.com>
2018-11-11 02:48:00 +0100

    Making the path work, WARNING : need to download the pathlib library with : "pip install pathlib" for this to work properly

--- app/server.py
+++ app/server.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template, jsonify
 from flask_restful import Resource, Api
+from pathlib import Path
 import json
+
 
 app = Flask(__name__)
 api = Api(app)
+data_folder = Path(__file__).resolve().parents[1] / "data/"
 
 @app.route("/")
 def main():
@@ -11,7 +14,8 @@ def main():
 
 @app.route("/stations")
 def get_stations():
-    with open('../data/stations_location.json') as json_data:
+    file_to_read = data_folder / "stations_location.json"
+    with open(str(file_to_read)) as json_data:
         stationsd = json.load(json_data)
     stations = sorted(list(map(lambda x: (x['nom'],x['geo']), stationsd)))
     locations = []
@@ -31,14 +35,14 @@ def get_station(station_name):
         juned = json.load(junedata)
     jnstations = list(filter(lambda x: x['station'] == station_name, juned))
     jndates = list(map(lambda x: x['date'][-2:], jnstations))
-    
+
     #Intersection : We don't have the same dates for January and June
     dates = list(filter(lambda x: x in jrdates, jndates))
     jrnumbers = list(map(lambda x: x['number'], list(filter(lambda x: x['date'][-2:] in dates, jrstations))))
     jnnumbers = list(map(lambda x: x['number'], list(filter(lambda x: x['date'][-2:] in dates, jnstations))))
-    
+
     data = {'station': station_name, 'dates': dates, 'jrNB': jrnumbers, 'jnNB': jnnumbers}
-    return render_template('station.html', data=data)     
+    return render_template('station.html', data=data)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
